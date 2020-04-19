8e939d74140803bcc531f48f25142dc7ad79b56e
dfsfdsfdsfsd <59228985+dfsfdsfdsfsd@users.noreply.github.com>
2020-04-19 17:40:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 	header('Content-Type: application/javascript');
 
-	$campaignId = 'a31q7d';
+	$campaignId = '314hfk';
 	$phpUrl = (is_https() ? "https://" : "http://"). $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'];
 
 	function is_https()
